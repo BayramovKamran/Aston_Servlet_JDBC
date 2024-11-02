7052150e891fa6d5e37334f6641332c74469fbaa
kamranbayramov <kamran.07@icloud.ru>
2024-11-02 12:23:27 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Aston_Servlet_JDBC
---
+++ src/main/java/org/example/model/Student.java
@@ -0,0 +1,66 @@
+package org.example.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private final List<Course> courses;
+
+    public Student() {
+        this.courses = new ArrayList<>();
+    }
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.courses = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void addCourse(Course course) {
+        courses.add(course);
+    }
+
+    public void removeCourse(Course course) {
+        courses.remove(course);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return id == student.id &&
+                Objects.equals(name, student.name) &&
+                Objects.equals(courses, student.courses);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, courses);
+    }
+}
+
